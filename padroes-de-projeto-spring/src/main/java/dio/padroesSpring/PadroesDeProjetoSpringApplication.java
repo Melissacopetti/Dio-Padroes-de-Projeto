4a5b7770bc03eb702bceaf89a408a372f9bb1c59
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2023-10-22 18:03:52 -0300

    criado projeto padroes-de-projeto-spring

---
+++ padroes-de-projeto-spring/src/main/java/dio/padroesSpring/PadroesDeProjetoSpringApplication.java
@@ -0,0 +1,13 @@
+package dio.padroesSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PadroesDeProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PadroesDeProjetoSpringApplication.class, args);
+	}
+
+}
